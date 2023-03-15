8fec6cc4054b69b6bdd27c3615fbe6766d3d449e
疯狂的狮子li <15040126243@163.com>
2023-03-15 17:17:48 +0800

    update 更新 readme 说明 更改框架定位

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,7 +2,7 @@
   <div class="app-container home">
     <el-row :gutter="20">
       <el-col :sm="24" :lg="12" style="padding-left: 20px">
-        <h2>RuoYi-Vue-Plus后台管理系统</h2>
+        <h2>RuoYi-Vue-Plus多租户管理系统</h2>
         <p>
           RuoYi-Vue-Plus 是基于 RuoYi-Vue 针对 分布式集群 场景升级(不兼容原框架)
           <br/>
