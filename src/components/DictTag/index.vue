92b2b489a40121a98417cabcfe644ddf80d039cd
疯狂的狮子Li <15040126243@163.com>
2023-09-14 17:22:36 +0800

    fix 修复 自定义字典样式不生效的问题

--- src/components/DictTag/index.vue
+++ src/components/DictTag/index.vue
@@ -2,7 +2,8 @@
   <div>
     <template v-for="(item, index) in options">
       <template v-if="values.includes(item.value)">
-        <span v-if="item.elTagType == 'default' || item.elTagType == ''" :key="item.value" :index="index" :class="item.elTagClass">
+        <span v-if="(item.elTagType == 'default' || item.elTagType == '') && (item.elTagClass == '' || item.elTagClass == null)"
+              :key="item.value" :index="index" :class="item.elTagClass">
           {{ item.label + " " }}
         </span>
         <el-tag
