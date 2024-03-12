eb364873d236e6bd0aec2c7ad8ad6bdaeafe75f3
dhb52 <dhb52@126.com>
2024-03-13 01:18:49 +0800

    ✨ EasyRetry Server 扩展模块

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -13,8 +13,9 @@ interface ImportMetaEnv {
   VITE_APP_CONTEXT_PATH: string;
   VITE_APP_MONITRO_ADMIN: string;
   VITE_APP_POWERJOB_ADMIN: string;
+  VITE_APP_EASYRETRY_ADMIN: string;
   VITE_APP_ENV: string;
-  VITE_APP_ENCRYPT: string
+  VITE_APP_ENCRYPT: string;
   VITE_APP_RSA_PUBLIC_KEY: string;
   VITE_APP_RSA_PRIVATE_KEY: string;
   VITE_APP_CLIENT_ID: string;
