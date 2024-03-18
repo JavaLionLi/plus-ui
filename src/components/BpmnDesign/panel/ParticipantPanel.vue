12458177beca5e17d68d74935751bf7add47f31b
LiuHao <liuhaoai545@gmail.com>
2024-03-18 13:56:16 +0800

    update 更新开始面板和泳道面板样式

--- src/components/BpmnDesign/panel/ParticipantPanel.vue
+++ src/components/BpmnDesign/panel/ParticipantPanel.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
-    <el-form ref="formRef" :model="formData" :rules="formRules" label-width="80px">
-      <el-form-item prop="id" label="节点 ID">
-        <el-input v-model="formData.id" @change="idChange"> </el-input>
-      </el-form-item>
-      <el-form-item prop="name" label="节点名称">
-        <el-input v-model="formData.name" @change="nameChange"> </el-input>
-      </el-form-item>
-      <el-form-item label="执行监听器" style="margin-bottom: 0"> </el-form-item>
-      <ExecutionListener :element="element"></ExecutionListener>
-    </el-form>
+    <el-collapse v-model="currentCollapseItem">
+      <el-collapse-item name="1">
+        <template #title>
+          <div class="collapse__title">
+            <el-icon>
+              <InfoFilled />
+            </el-icon>
+            常规
+          </div>
+        </template>
+        <div>
+          <el-form ref="formRef" :model="formData" :rules="formRules" label-width="90px">
+            <el-form-item prop="id" label="节点 ID">
+              <el-input v-model="formData.id" @change="idChange"> </el-input>
+            </el-form-item>
+            <el-form-item prop="name" label="节点名称">
+              <el-input v-model="formData.name" @change="nameChange"> </el-input>
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-collapse-item>
+
+      <el-collapse-item name="2">
+        <template #title>
+          <div class="collapse__title">
+            <el-icon>
+              <BellFilled />
+            </el-icon>
+            执行监听器
+          </div>
+        </template>
+        <div>
+          <ExecutionListener :element="element"></ExecutionListener>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
   </div>
 </template>
 <script setup lang="ts">
@@ -30,7 +56,7 @@ const { parseData } = useParseElement({
 });
 
 const formData = ref(parseData<StartEndPanel>());
-
+const currentCollapseItem = ref(['1', '2']);
 const formRules = ref<ElFormRules>({
   id: [{ required: true, message: '请输入', trigger: 'blur' }],
   name: [{ required: true, message: '请输入', trigger: 'blur' }]
