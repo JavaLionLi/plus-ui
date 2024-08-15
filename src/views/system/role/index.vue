e5727893bbfb166118798edcdfed805d77c3f606
疯狂的狮子Li <15040126243@163.com>
2024-08-15 18:05:19 +0800

    update 优化 时间搜索组件统一

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -18,7 +18,7 @@
             <el-form-item label="创建时间" style="width: 308px">
               <el-date-picker
                 v-model="dateRange"
-                value-format="YYYY-MM-DD"
+                value-format="YYYY-MM-DD HH:mm:ss"
                 type="daterange"
                 range-separator="-"
                 start-placeholder="开始日期"
