4918552492723d00a846ce2d552cf8888c5a5f32
疯狂的狮子Li <15040126243@163.com>
2024-10-12 10:58:23 +0800

    fix 修复 请假日期选择格式不对问题

--- src/views/workflow/leave/leaveEdit.vue
+++ src/views/workflow/leave/leaveEdit.vue
@@ -23,10 +23,12 @@
         <el-form-item label="请假时间">
           <el-date-picker
             v-model="leaveTime"
+            value-format="YYYY-MM-DD HH:mm:ss"
             type="daterange"
             range-separator="To"
             start-placeholder="开始时间"
             end-placeholder="结束时间"
+            :default-time="[new Date(2000, 1, 1, 0, 0, 0), new Date(2000, 1, 1, 23, 59, 59)]"
             @change="changeLeaveTime()"
           />
         </el-form-item>
